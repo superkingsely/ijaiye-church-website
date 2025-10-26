a9766f7021217beb4cd2530da065298ca3b30644
superkingsely <superkingsely@gmail.com>
2025-10-26 13:16:15 +0100

    good

--- src/presentation/components/Header.tsx
+++ src/presentation/components/Header.tsx
@@ -3,6 +3,7 @@
 import React, { useEffect, useState } from 'react'
 import { NavUrl, useNavStore } from '../stores/navstore';
 import Link from 'next/link';
+import { Home } from 'lucide-react';
 
 const Header = () => {
 
@@ -20,15 +21,15 @@ const Header = () => {
     },[setScrolled])
 
   return (
-    <header className='fixed top-0 w-full ' >
-        <div className={`container  border mx-auto flex justify-between items-center py-4 px-6 transition-all duration-300 ${scrolled ? ' bg-pink-600  shadow-md' : 'bg-transparent'}`}>
+    <header className='fixed top-0 w-full text-white ' >
+        <div className={`container relative border mx-auto flex justify-between items-center py-4 px-6 transition-all duration-300 ${scrolled ? ' bg-pink-600  shadow-md' : 'bg-transparent'}`}>
             <div className="logo text-xl md:2xl font-bold">Church Logo</div>
-            <nav className='desktop hidden md:visible '  >
-                <ul className="flex space-x-4">
+            <nav className='desktop hidden md:visible  '  >
+                <ul className="flex space-x-4 text-white ">
                    {
                     items.map((item: NavUrl) => (
                         <li key={item.name} className="relative group ">
-                            <Link href={item.url ? item.url : '#'} className="text-black hover:text-gray-300">{item.name}</Link>
+                            <Link href={item.url ? item.url : '#'} className="text-white hover:text-gray-300">{item.name}</Link>
                             {item.submenu && (
 
                                 <ul className="ml-4 absolute top-full left-0 border cursor scale-0 px-[15px] py-[10px] group-hover:scale-100 bg-white " >
@@ -43,6 +44,28 @@ const Header = () => {
                    ))}
                 </ul>
             </nav>
+            {/* mobile */}
+            <nav className='mobile md:hidden absolute top-0  right-0 border w-[50vw] h-[100vh] hidden '  >
+                <ul className="flex flex-col space-y-4 border  ">
+                   {
+                    items.map((item: NavUrl) => (
+                        <li key={item.name} className="relative group  ">
+                            <Link href={item.url ? item.url : '#'} className=" hover:text-gray-300 text-white ">{item.name}</Link>
+                            {item.submenu && (
+
+                                <ul className="ml-4 absolute top-full left-0 border cursor scale-0 px-[15px] py-[10px] group-hover:scale-100 bg-white " >
+                                    {item.submenu.map((subitem) => (
+                                        <li key={subitem.name}>
+                                            <Link href={subitem.url ? subitem.url : '#'} className="text-black hover:text-gray-300">{subitem.name}</Link>
+                                        </li>
+                                    ))}
+                                </ul>
+                            )}
+                        </li>
+                   ))}
+                </ul>
+            </nav>
+            <Home className="w-6 h-6 text-white-600 md:hidden " />
         </div>
     </header>
   )
