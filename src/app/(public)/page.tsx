bfde8a4e7ba044d3e270a5a09caca58582f7840b
superkingsely <superkingsely@gmail.com>
2025-10-29 12:00:41 +0100

    okay

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,10 +1,13 @@
+'use client'
 import AboutSection from '@/presentation/components/AboutSection'
 import HeroSection from '@/presentation/components/HeroSection'
+import { useNavStore } from '@/presentation/stores/navstore'
 import React from 'react'
 
 const Page = () => {
+  const {toggle}=useNavStore()
   return (
-    <div className="border">
+    <div onClick={toggle} className="border">
       <HeroSection />
       <AboutSection />
     </div>
