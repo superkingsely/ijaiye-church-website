e8e1664814b4251873b05b64793ad95a9b6ef834
superkingsely <superkingsely@gmail.com>
2025-11-05 17:01:39 +0100

    good

--- src/app/(public)/give/page.tsx
+++ src/app/(public)/give/page.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 const Page = () => {
   return (
     <section className='  w-full' >
-        <SectionBanner title="GIVE" subtitle="" />
+        <SectionBanner title="GIVE" subtitle="Pay your offerings, tithes, first fruit offering etc via any of the listed platforms." />
         <div className="section-content give-section-content max-w-[1200px] mx-auto p-6 min-h-[80vh] ">
             <h1 className='text-3xl font-bold mb-4'>GIVE</h1>
         </div>
