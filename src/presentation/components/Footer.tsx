e646c8016d86c55efc7adcea097239c12c9865bd
superkingsely <superkingsely@gmail.com>
2025-11-05 14:37:23 +0100

    okay

--- src/presentation/components/Footer.tsx
+++ src/presentation/components/Footer.tsx
@@ -2,10 +2,15 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer>
-        <div className="footer-content">
-            
+    <footer className='' >
+        <div className="footer-content max-w-[1200px] mx-auto ">
+            <div className="ads-footer">
+
+            </div>
         </div>
+            <div className="copyright min-h-[10vh] bg-purple-600 flex items-center justify-center ">
+                <p className='text-white text-center py-4'>© 2025 Mountain of Fire and Miracles Ministries, Ijaiye Region 4. All rights reserved.</p>
+            </div>
     </footer>
   )
 }
